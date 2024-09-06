7af9a9f042a38d64e37ffe9343b55b703f42ea15
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-09-06 15:55:35 -0300

    feat: configurar o spring security para token jwt

--- src/main/java/com/ednaldo/springsecurity6/repositories/TweetRepository.java
+++ src/main/java/com/ednaldo/springsecurity6/repositories/TweetRepository.java
@@ -1,4 +1,4 @@
-package com.ednaldo.springsecurity6.entities.repositories;
+package com.ednaldo.springsecurity6.repositories;
 
 import com.ednaldo.springsecurity6.entities.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
